a3bf14889ac9b2e9ddf34afc3df57eb0c1bfd6fa
BananaDevVr <140212712+BananaDevVr@users.noreply.github.com>
2024-01-07 20:10:29 -0500

    Update JoinNow.js

--- src/components/JoinNow.js
+++ src/components/JoinNow.js
@@ -1,6 +1,9 @@
 import Link from "next/link";
 
 const JoinNow = () => {
+  // Replace 'YOUR_INVITE_LINK' with the actual invite link
+  const inviteLink = 'https://discord.com/api/oauth2/authorize?client_id=1193383659122741291&permissions=8&scope=bot';
+
   return (
     <>
       <section className="joinUs">
@@ -8,11 +11,13 @@ const JoinNow = () => {
           <h3>Join us now!</h3>
         </div>
         <div className="joinUs-para p-color">
-          Invite our bot to your Discord server!{" "}
+          Invite our bot to your Discord server!
         </div>
         <button className="btn">
-          <Link className="h-color no-decoration" href="#">
-            Invite now!
+          {/* Use the Link component to create a link to the invite */}
+          <Link href={inviteLink}>
+            {/* Add styling and text for the link */}
+            <a className="h-color no-decoration">Invite now!</a>
           </Link>
         </button>
       </section>
